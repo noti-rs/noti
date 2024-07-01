aa5f00fe03eab30ad7801411142e9325a94a2d44
Pavel <work.belyavsky.p.a@gmail.com>
2024-07-02 02:10:11 +0300

    refactor: split single internal channel to two sides for avoid Arc<Mutex<T>>

--- src/render.rs
+++ src/render.rs
@@ -1,10 +1,10 @@
-use std::sync::{Arc, Mutex};
-
 use wayland_client::Connection;
 
 use crate::data::{
     aliases::Result,
-    internal_messages::{InternalChannel, ServerMessage},
+    internal_messages::{
+        InternalChannel, RendererInternalChannel, ServerInternalChannel, ServerMessage,
+    },
 };
 
 use self::layer::NotificationStack;
@@ -15,36 +15,34 @@ mod layer;
 pub(crate) struct Renderer {
     connection: Connection,
     notification_stack: NotificationStack,
-    channel: Arc<Mutex<InternalChannel>>,
+    channel: RendererInternalChannel,
 }
 
 impl Renderer {
-    pub(crate) fn init() -> Result<Self> {
+    pub(crate) fn init() -> Result<(ServerInternalChannel, Self)> {
         let connection = Connection::connect_to_env()?;
 
-        Ok(Self {
-            connection,
-            notification_stack: NotificationStack::init()?,
-            channel: Arc::new(Mutex::new(InternalChannel::new())),
-        })
-    }
-
-    pub(crate) fn clone_channel(&self) -> Arc<Mutex<InternalChannel>> {
-        self.channel.clone()
+        let (server_internal_channel, renderer_internal_channel) = InternalChannel::new().split();
+        Ok((
+            server_internal_channel,
+            Self {
+                connection,
+                notification_stack: NotificationStack::init()?,
+                channel: renderer_internal_channel,
+            },
+        ))
     }
 
     pub(crate) fn run(&mut self) {
         loop {
-            if let Ok(channel) = self.channel.try_lock() {
-                if let Ok(message) = channel.try_recv_from_server() {
-                    match message {
-                        ServerMessage::ShowNotification(notification) => {
-                            dbg!(&notification);
-                            self.notification_stack
-                                .create_notification_rect(notification);
-                        }
-                        ServerMessage::CloseNotification { id } => todo!(),
+            if let Ok(message) = self.channel.try_recv_from_server() {
+                match message {
+                    ServerMessage::ShowNotification(notification) => {
+                        dbg!(&notification);
+                        self.notification_stack
+                            .create_notification_rect(notification);
                     }
+                    ServerMessage::CloseNotification { id } => todo!(),
                 }
             }
 
