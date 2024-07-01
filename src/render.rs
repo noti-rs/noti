910a915bc2cc1dda46c26f66802213030235ed75
Pavel <work.belyavsky.p.a@gmail.com>
2024-07-02 01:16:10 +0300

    feat: add minimal render of notification

--- src/render.rs
+++ src/render.rs
@@ -1,45 +1,54 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
-use smithay_client_toolkit::reexports::client::Connection;
+use wayland_client::Connection;
 
 use crate::data::{
     aliases::Result,
     internal_messages::{InternalChannel, ServerMessage},
 };
-use crate::render::layer::NotificationLayer;
+
+use self::layer::NotificationStack;
 
 mod font;
 mod layer;
 
-struct Renderer {
+pub(crate) struct Renderer {
     connection: Connection,
-    layers: Vec<NotificationLayer>,
-    channel: Arc<InternalChannel>,
+    notification_stack: NotificationStack,
+    channel: Arc<Mutex<InternalChannel>>,
 }
 
 impl Renderer {
-    fn init() -> Result<Self> {
+    pub(crate) fn init() -> Result<Self> {
         let connection = Connection::connect_to_env()?;
 
         Ok(Self {
             connection,
-            layers: vec![],
-            channel: Arc::new(InternalChannel::new()),
+            notification_stack: NotificationStack::init()?,
+            channel: Arc::new(Mutex::new(InternalChannel::new())),
         })
     }
 
-    fn clone_channel(&self) -> Arc<InternalChannel> {
+    pub(crate) fn clone_channel(&self) -> Arc<Mutex<InternalChannel>> {
         self.channel.clone()
     }
 
-    fn run(&mut self) {
+    pub(crate) fn run(&mut self) {
         loop {
-            if let Ok(message) = self.channel.try_recv_from_server() {
-                match message {
-                    ServerMessage::ShowNotification(notification) => todo!(),
-                    ServerMessage::CloseNotification { id } => todo!(),
+            if let Ok(channel) = self.channel.try_lock() {
+                if let Ok(message) = channel.try_recv_from_server() {
+                    match message {
+                        ServerMessage::ShowNotification(notification) => {
+                            dbg!(&notification);
+                            self.notification_stack
+                                .create_notification_rect(notification);
+                        }
+                        ServerMessage::CloseNotification { id } => todo!(),
+                    }
                 }
             }
+
+            self.notification_stack.dispatch();
         }
     }
 }
--- src/render/layer.rs
+++ src/render/layer.rs
@@ -1,31 +1,328 @@
-use smithay_client_toolkit::{
-    output::OutputState,
-    reexports::client::protocol::wl_pointer,
-    registry::RegistryState,
-    seat::SeatState,
-    shell::wlr_layer::LayerSurface,
-    shm::{slot::SlotPool, Shm},
+use std::{fs::File, io::Write, os::fd::AsFd};
+
+use crate::data::{aliases::Result, notification::Notification};
+use wayland_client::{
+    delegate_noop,
+    protocol::{
+        wl_buffer, wl_compositor,
+        wl_pointer::{self, ButtonState},
+        wl_registry, wl_seat, wl_shm, wl_shm_pool, wl_surface,
+    },
+    Connection, Dispatch, EventQueue, WEnum,
+};
+use wayland_protocols_wlr::layer_shell::v1::client::{
+    zwlr_layer_shell_v1,
+    zwlr_layer_surface_v1::{self, Anchor},
 };
 
-pub(crate) struct NotificationLayer {
-    registry_state: RegistryState,
-    seat_state: SeatState,
-    output_state: OutputState,
-    shm: Shm,
+pub(crate) struct NotificationStack {
+    connection: Connection,
 
-    close: bool,
-    pool: SlotPool,
-    width: u32,
-    height: u32,
-    layer: LayerSurface,
-    pointer: Option<wl_pointer::WlPointer>,
-
-    margin: Margin,
+    stack: Vec<(EventQueue<NotificationRect>, NotificationRect)>,
 }
 
-pub(crate) struct Margin {
+impl NotificationStack {
+    pub(crate) fn init() -> Result<Self> {
+        let connection = Connection::connect_to_env()?;
+
+        Ok(Self {
+            connection,
+            stack: vec![],
+        })
+    }
+
+    pub(crate) fn create_notification_rect(&mut self, notification: Notification) {
+        let event_queue = self.connection.new_event_queue();
+        let qh = event_queue.handle();
+        let display = self.connection.display();
+
+        display.get_registry(&qh, ());
+        let notification_rect = NotificationRect::init(notification);
+
+        self.stack.push((event_queue, notification_rect));
+    }
+
+    pub(crate) fn dispatch(&mut self) {
+        self.stack
+            .iter_mut()
+            .for_each(|(event_queue, notification_rect)| {
+                event_queue
+                    .blocking_dispatch(notification_rect)
+                    .expect("Succesful dispatching");
+            });
+    }
+}
+
+struct NotificationRect {
+    width: i32,
+    height: i32,
+    margin: Margin,
+
+    data: Notification,
+
+    shm: Option<wl_shm::WlShm>,
+    buffer: Option<wl_buffer::WlBuffer>,
+
+    surface: Option<wl_surface::WlSurface>,
+    layer_surface: Option<zwlr_layer_surface_v1::ZwlrLayerSurfaceV1>,
+
+    configured: bool,
+    alive: bool,
+
+    pointer_state: PointerState,
+}
+
+impl NotificationRect {
+    fn init(notification: Notification) -> Self {
+        //TODO: change to values from config
+        Self {
+            width: 300,
+            height: 150,
+            margin: Margin::new(),
+            data: notification,
+
+            shm: None,
+            buffer: None,
+
+            surface: None,
+            layer_surface: None,
+
+            configured: false,
+            alive: true,
+
+            pointer_state: Default::default(),
+        }
+    }
+
+    fn draw(&self, tmp: &mut File) {
+        let mut buf = std::io::BufWriter::new(tmp);
+        (0..self.width * self.height).for_each(|_| buf.write_all(&[255, 255, 255, 255]).unwrap());
+        buf.flush().unwrap();
+    }
+}
+
+struct Margin {
     left: i32,
     right: i32,
     top: i32,
     bottom: i32,
+}
+
+impl Margin {
+    fn new() -> Self {
+        Margin {
+            left: 0,
+            right: 25,
+            top: 25,
+            bottom: 0,
+        }
+    }
+
+    fn apply(&self, layer_surface: &zwlr_layer_surface_v1::ZwlrLayerSurfaceV1) {
+        layer_surface.set_margin(self.top, self.right, self.bottom, self.left);
+    }
+}
+
+#[derive(Default)]
+struct PointerState {
+    x: f64,
+    y: f64,
+}
+
+impl Dispatch<wl_registry::WlRegistry, ()> for NotificationRect {
+    fn event(
+        state: &mut Self,
+        registry: &wl_registry::WlRegistry,
+        event: <wl_registry::WlRegistry as wayland_client::Proxy>::Event,
+        _data: &(),
+        _conn: &wayland_client::Connection,
+        qhandle: &wayland_client::QueueHandle<Self>,
+    ) {
+        if let wl_registry::Event::Global {
+            name,
+            interface,
+            version,
+        } = event
+        {
+            match &interface[..] {
+                "wl_compositor" => {
+                    let compositor = registry.bind::<wl_compositor::WlCompositor, _, _>(
+                        name,
+                        version,
+                        qhandle,
+                        (),
+                    );
+                    state.surface = Some(compositor.create_surface(qhandle, ()));
+                }
+                "wl_shm" => {
+                    state.shm =
+                        Some(registry.bind::<wl_shm::WlShm, _, _>(name, version, qhandle, ()))
+                }
+                "wl_seat" => {
+                    registry.bind::<wl_seat::WlSeat, _, _>(name, version, qhandle, ());
+                }
+                "zwlr_layer_shell_v1" => {
+                    let layer_shell = registry.bind::<zwlr_layer_shell_v1::ZwlrLayerShellV1, _, _>(
+                        name,
+                        version,
+                        qhandle,
+                        (),
+                    );
+                    if let Some(surface) = state.surface.as_ref() {
+                        let layer_surface = layer_shell.get_layer_surface(
+                            surface,
+                            None,
+                            zwlr_layer_shell_v1::Layer::Overlay,
+                            "notification-layer".to_string(),
+                            qhandle,
+                            (),
+                        );
+
+                        state.margin.apply(&layer_surface);
+                        layer_surface.set_size(state.width as u32, state.height as u32);
+                        layer_surface.set_keyboard_interactivity(
+                            zwlr_layer_surface_v1::KeyboardInteractivity::None,
+                        );
+                        layer_surface.set_anchor(Anchor::Top.union(Anchor::Right));
+                        surface.commit();
+
+                        state.layer_surface = Some(layer_surface);
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+}
+
+delegate_noop!(NotificationRect: ignore wl_compositor::WlCompositor);
+delegate_noop!(NotificationRect: ignore wl_surface::WlSurface);
+delegate_noop!(NotificationRect: ignore zwlr_layer_shell_v1::ZwlrLayerShellV1);
+delegate_noop!(NotificationRect: ignore wl_shm_pool::WlShmPool);
+delegate_noop!(NotificationRect: ignore wl_buffer::WlBuffer);
+
+impl Dispatch<wl_shm::WlShm, ()> for NotificationRect {
+    fn event(
+        state: &mut Self,
+        shm: &wl_shm::WlShm,
+        _event: <wl_shm::WlShm as wayland_client::Proxy>::Event,
+        _data: &(),
+        _conn: &Connection,
+        qhandle: &wayland_client::QueueHandle<Self>,
+    ) {
+        let mut file = tempfile::tempfile().unwrap();
+        state.draw(&mut file);
+        let pool = shm.create_pool(file.as_fd(), state.width * state.height * 4, qhandle, ());
+        state.buffer = Some(pool.create_buffer(
+            0,
+            state.width,
+            state.height,
+            4,
+            wl_shm::Format::Argb8888,
+            qhandle,
+            (),
+        ));
+    }
+}
+
+impl Dispatch<wl_seat::WlSeat, ()> for NotificationRect {
+    fn event(
+        _state: &mut Self,
+        seat: &wl_seat::WlSeat,
+        event: <wl_seat::WlSeat as wayland_client::Proxy>::Event,
+        _data: &(),
+        _conn: &wayland_client::Connection,
+        qhandle: &wayland_client::QueueHandle<Self>,
+    ) {
+        if let wl_seat::Event::Capabilities {
+            capabilities: WEnum::Value(capability),
+        } = event
+        {
+            if capability.contains(wl_seat::Capability::Pointer) {
+                seat.get_pointer(qhandle, ());
+            }
+        }
+    }
+}
+
+impl Dispatch<wl_pointer::WlPointer, ()> for NotificationRect {
+    fn event(
+        state: &mut Self,
+        _pointer: &wl_pointer::WlPointer,
+        event: <wl_pointer::WlPointer as wayland_client::Proxy>::Event,
+        _data: &(),
+        _conn: &wayland_client::Connection,
+        _qhandle: &wayland_client::QueueHandle<Self>,
+    ) {
+        match event {
+            wl_pointer::Event::Enter {
+                surface_x,
+                surface_y,
+                ..
+            } => {
+                (state.pointer_state.x, state.pointer_state.y) = (surface_x, surface_y);
+            }
+            wl_pointer::Event::Leave { .. } => (),
+            wl_pointer::Event::Motion {
+                surface_x,
+                surface_y,
+                ..
+            } => {
+                (state.pointer_state.x, state.pointer_state.y) = (surface_x, surface_y);
+            }
+            wl_pointer::Event::Button {
+                button,
+                state: WEnum::Value(ButtonState::Pressed),
+                ..
+            } => {
+                const LEFT_BTN: u32 = 272;
+                const RIGHT_BTN: u32 = 273;
+                const MIDDLE_BTN: u32 = 274;
+
+                match button {
+                    LEFT_BTN => (),
+                    RIGHT_BTN => (),
+                    MIDDLE_BTN => (),
+                    _ => (),
+                }
+            }
+            // wl_pointer::Event::Frame => todo!(),
+            _ => (),
+        }
+    }
+}
+
+impl Dispatch<zwlr_layer_surface_v1::ZwlrLayerSurfaceV1, ()> for NotificationRect {
+    fn event(
+        state: &mut Self,
+        layer_surface: &zwlr_layer_surface_v1::ZwlrLayerSurfaceV1,
+        event: <zwlr_layer_surface_v1::ZwlrLayerSurfaceV1 as wayland_client::Proxy>::Event,
+        _data: &(),
+        _conn: &wayland_client::Connection,
+        _qhandle: &wayland_client::QueueHandle<Self>,
+    ) {
+        if let zwlr_layer_surface_v1::Event::Configure {
+            serial,
+            width,
+            height,
+        } = event
+        {
+            layer_surface.ack_configure(serial);
+
+            if width == 0 && height == 0 {
+                //TODO: change to values from config
+                state.width = 300;
+                state.height = 150;
+            } else {
+                state.width = width as i32;
+                state.height = height as i32;
+            }
+            state.configured = true;
+
+            if let Some(surface) = state.surface.as_ref() {
+                surface.attach(state.buffer.as_ref(), 0, 0);
+                surface.commit();
+            }
+        }
+    }
 }
