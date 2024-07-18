93ba48b31ab57bfeffcb2ccff715b1db112dc51a
Pavel <work.belyavsky.p.a@gmail.com>
2024-07-18 12:52:05 +0300

    fix: add missing anchor to correct indexing

--- src/render/layer.rs
+++ src/render/layer.rs
@@ -182,8 +182,9 @@ impl NotificationStack {
                 let rect_height = CONFIG.general().height() as usize;
                 let gap = CONFIG.general().gap() as usize;
                 let count = window.height as usize / rect_height;
+                let anchor = CONFIG.general().anchor();
 
-                if let Some((i, _)) = (0..window.height as usize)
+                if let Some(i) = (0..window.height as usize)
                     .step_by(rect_height + gap)
                     .enumerate()
                     .take(count)
@@ -191,6 +192,7 @@ impl NotificationStack {
                         let rect_bottom = rect_top + rect_height;
                         (rect_top..rect_bottom).contains(&(window.pointer_state.y as usize))
                     })
+                    .map(|(i, _)| if anchor.is_top() { count - i - 1 } else { i })
                 {
                     let notifications = self.remove_rects(&[i]);
                     notifications.into_iter().for_each(|notification| {
