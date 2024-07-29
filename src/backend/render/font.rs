0b794d8ba163fbeace4e1795b35916c162846820
Pavel <work.belyavsky.p.a@gmail.com>
2024-07-29 14:11:49 +0300

    refactor: simplify `FontStyle` enum by removing redundant

--- src/backend/render/font.rs
+++ src/backend/render/font.rs
@@ -250,34 +250,6 @@ pub(crate) enum FontStyle {
     Italic,
     #[display(fmt = "BoldItalic")]
     BoldItalic,
-    #[display(fmt = "Medium")]
-    Medium,
-    #[display(fmt = "MediumItalic")]
-    MediumItalic,
-    #[display(fmt = "Light")]
-    Light,
-    #[display(fmt = "LightItalic")]
-    LightItalic,
-    #[display(fmt = "Thin")]
-    Thin,
-    #[display(fmt = "ThinItalic")]
-    ThinItalic,
-    #[display(fmt = "SemiBold")]
-    SemiBold,
-    #[display(fmt = "SemiBoldItalic")]
-    SemiBoldItalic,
-    #[display(fmt = "ExtraBold")]
-    ExtraBold,
-    #[display(fmt = "ExtraLight")]
-    ExtraLight,
-    #[display(fmt = "ExtraBoldItalic")]
-    ExtraBoldItalic,
-    #[display(fmt = "ExtraLightItalic")]
-    ExtraLightItalic,
-    #[display(fmt = "Black")]
-    Black,
-    #[display(fmt = "BlacItalic")]
-    BlackItalic,
 }
 
 impl From<&str> for FontStyle {
@@ -287,21 +259,7 @@ impl From<&str> for FontStyle {
             "Bold" => Self::Bold,
             "Italic" => Self::Italic,
             "Bold Italic" => Self::BoldItalic,
-            "Medium" => Self::Medium,
-            "Medium Italic" => Self::MediumItalic,
-            "SemiBold" => Self::SemiBold,
-            "SemiBold Italic" => Self::SemiBoldItalic,
-            "Light" => Self::Light,
-            "Light Italic" => Self::LightItalic,
-            "Thin" => Self::Thin,
-            "Thin Italic" => Self::ThinItalic,
-            "ExtraBold" => Self::ExtraBold,
-            "ExtraBold Italic" => Self::ExtraBoldItalic,
-            "ExtraLight" => Self::ExtraLight,
-            "ExtraLight Italic" => Self::ExtraLightItalic,
-            "Black" => Self::Black,
-            "Black Italic" => Self::BlackItalic,
-            other => panic!("Invalid style: {other}"),
+            other => panic!("Unsupported style: {other}"),
         }
     }
 }
@@ -327,128 +285,13 @@ fn union_font_styles(lhs: &FontStyle, rhs: &FontStyle) -> FontStyle {
         FontStyle::Bold => match rhs {
             FontStyle::Regular | FontStyle::Bold => FontStyle::Bold,
             FontStyle::Italic | FontStyle::BoldItalic => FontStyle::BoldItalic,
-            FontStyle::ExtraBold => FontStyle::ExtraBold,
-            FontStyle::ExtraBoldItalic => FontStyle::ExtraBoldItalic,
-            FontStyle::SemiBold => FontStyle::SemiBold,
-            FontStyle::SemiBoldItalic => FontStyle::SemiBoldItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
         },
         FontStyle::Italic => match rhs {
             FontStyle::Regular | FontStyle::Italic => FontStyle::Italic,
             FontStyle::Bold | FontStyle::BoldItalic => FontStyle::BoldItalic,
-            FontStyle::ExtraLight | FontStyle::ExtraLightItalic => FontStyle::ExtraLightItalic,
-            FontStyle::ExtraBold | FontStyle::ExtraBoldItalic => FontStyle::ExtraBoldItalic,
-            FontStyle::Light | FontStyle::LightItalic => FontStyle::LightItalic,
-            FontStyle::Thin | FontStyle::ThinItalic => FontStyle::ThinItalic,
-            FontStyle::SemiBold | FontStyle::SemiBoldItalic => FontStyle::SemiBoldItalic,
-            FontStyle::Medium | FontStyle::MediumItalic => FontStyle::MediumItalic,
-            FontStyle::Black | FontStyle::BlackItalic => FontStyle::BlackItalic,
         },
-        FontStyle::BoldItalic => match rhs {
-            FontStyle::Regular | FontStyle::Bold | FontStyle::Italic | FontStyle::BoldItalic => {
-                FontStyle::BoldItalic
-            }
-            FontStyle::ExtraBold | FontStyle::ExtraBoldItalic => FontStyle::ExtraBoldItalic,
-            FontStyle::SemiBold | FontStyle::SemiBoldItalic => FontStyle::SemiBoldItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::ExtraBold => match rhs {
-            FontStyle::Regular | FontStyle::Bold | FontStyle::ExtraBold => FontStyle::ExtraBold,
-            FontStyle::Italic | FontStyle::BoldItalic | FontStyle::ExtraBoldItalic => {
-                FontStyle::ExtraBoldItalic
-            }
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::ExtraLight => match rhs {
-            FontStyle::Regular | FontStyle::Light | FontStyle::ExtraLight => FontStyle::ExtraLight,
-            FontStyle::Italic | FontStyle::ExtraLightItalic | FontStyle::LightItalic => {
-                FontStyle::ExtraLightItalic
-            }
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::ExtraLightItalic => match rhs {
-            FontStyle::Regular
-            | FontStyle::Italic
-            | FontStyle::ExtraLight
-            | FontStyle::ExtraLightItalic
-            | FontStyle::LightItalic
-            | FontStyle::Light => FontStyle::ExtraLightItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::ExtraBoldItalic => match rhs {
-            FontStyle::Regular
-            | FontStyle::Bold
-            | FontStyle::Italic
-            | FontStyle::BoldItalic
-            | FontStyle::ExtraBold
-            | FontStyle::ExtraBoldItalic => FontStyle::ExtraBoldItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::LightItalic => match rhs {
-            FontStyle::Regular | FontStyle::Italic | FontStyle::Light | FontStyle::LightItalic => {
-                FontStyle::LightItalic
-            }
-            FontStyle::ExtraLight | FontStyle::ExtraLightItalic => FontStyle::ExtraLightItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::Thin => match rhs {
-            FontStyle::Regular | FontStyle::Thin => FontStyle::Thin,
-            FontStyle::Italic | FontStyle::ThinItalic => FontStyle::ThinItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::ThinItalic => match rhs {
-            FontStyle::Regular | FontStyle::Thin | FontStyle::Italic | FontStyle::ThinItalic => {
-                FontStyle::ThinItalic
-            }
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::SemiBold => match rhs {
-            FontStyle::Regular | FontStyle::Bold | FontStyle::SemiBold => FontStyle::SemiBold,
-            FontStyle::Italic | FontStyle::BoldItalic | FontStyle::SemiBoldItalic => {
-                FontStyle::SemiBoldItalic
-            }
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::SemiBoldItalic => match rhs {
-            FontStyle::Regular
-            | FontStyle::Bold
-            | FontStyle::SemiBold
-            | FontStyle::Italic
-            | FontStyle::BoldItalic
-            | FontStyle::SemiBoldItalic => FontStyle::SemiBoldItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::Medium => match rhs {
-            FontStyle::Regular | FontStyle::Medium => FontStyle::Medium,
-            FontStyle::Italic | FontStyle::MediumItalic => FontStyle::MediumItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
+        FontStyle::BoldItalic => lhs.clone(),
         FontStyle::Regular => rhs.clone(),
-        FontStyle::Light => match rhs {
-            FontStyle::Regular | FontStyle::Light => FontStyle::Light,
-            FontStyle::Italic | FontStyle::LightItalic => FontStyle::LightItalic,
-            FontStyle::ExtraLight => FontStyle::ExtraLight,
-            FontStyle::ExtraLightItalic => FontStyle::ExtraLightItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::MediumItalic => match rhs {
-            FontStyle::Regular
-            | FontStyle::Medium
-            | FontStyle::Italic
-            | FontStyle::MediumItalic => FontStyle::MediumItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::Black => match rhs {
-            FontStyle::Regular | FontStyle::Black => FontStyle::Black,
-            FontStyle::Italic | FontStyle::BlackItalic => FontStyle::BlackItalic,
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
-        FontStyle::BlackItalic => match rhs {
-            FontStyle::Regular | FontStyle::Black | FontStyle::Italic | FontStyle::BlackItalic => {
-                FontStyle::BlackItalic
-            }
-            other => panic!("Incorrect combination of {lhs} and {other}"),
-        },
     }
 }
 
@@ -491,100 +334,9 @@ fn intersect_font_styles(lhs: &FontStyle, rhs: &FontStyle) -> FontStyle {
             FontStyle::Italic => FontStyle::Bold,
             FontStyle::BoldItalic => FontStyle::Regular,
             FontStyle::Regular => FontStyle::BoldItalic,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::ExtraBold => match rhs {
-            FontStyle::Bold => FontStyle::Bold,
-            FontStyle::ExtraBold => FontStyle::Regular,
-            FontStyle::Regular => FontStyle::ExtraBold,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::ExtraLight => match rhs {
-            FontStyle::Regular => FontStyle::ExtraLight,
-            FontStyle::ExtraLight => FontStyle::Regular,
-            FontStyle::Light => FontStyle::Light,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::ExtraLightItalic => match rhs {
-            FontStyle::Italic => FontStyle::ExtraLight,
-            FontStyle::ExtraLight => FontStyle::Italic,
-            FontStyle::ExtraLightItalic => FontStyle::Regular,
-            FontStyle::LightItalic => FontStyle::Light,
-            FontStyle::Regular => FontStyle::ExtraLightItalic,
-            FontStyle::Light => FontStyle::LightItalic,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::ExtraBoldItalic => match rhs {
-            FontStyle::Bold => FontStyle::BoldItalic,
-            FontStyle::Italic => FontStyle::ExtraBold,
-            FontStyle::BoldItalic => FontStyle::Bold,
-            FontStyle::ExtraBold => FontStyle::Italic,
-            FontStyle::ExtraBoldItalic => FontStyle::Regular,
-            FontStyle::Regular => FontStyle::ExtraBoldItalic,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::LightItalic => match rhs {
-            FontStyle::Italic => FontStyle::Light,
-            FontStyle::LightItalic => FontStyle::Regular,
-            FontStyle::Regular => FontStyle::LightItalic,
-            FontStyle::Light => FontStyle::Italic,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::Thin => match rhs {
-            FontStyle::Thin => FontStyle::Regular,
-            FontStyle::Regular => FontStyle::Thin,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::ThinItalic => match rhs {
-            FontStyle::Italic => FontStyle::Thin,
-            FontStyle::Thin => FontStyle::Italic,
-            FontStyle::ThinItalic => FontStyle::Regular,
-            FontStyle::Regular => FontStyle::ThinItalic,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::SemiBold => match rhs {
-            FontStyle::SemiBold => FontStyle::Regular,
-            FontStyle::Regular => FontStyle::SemiBold,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::SemiBoldItalic => match rhs {
-            FontStyle::Italic => FontStyle::SemiBold,
-            FontStyle::SemiBold => FontStyle::Italic,
-            FontStyle::SemiBoldItalic => FontStyle::Regular,
-            FontStyle::Regular => FontStyle::SemiBoldItalic,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::Medium => match rhs {
-            FontStyle::Medium => FontStyle::Regular,
-            FontStyle::Regular => FontStyle::Medium,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
         },
         FontStyle::Regular => match rhs {
             FontStyle::Regular => FontStyle::Regular,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::Light => match rhs {
-            FontStyle::Regular => FontStyle::Light,
-            FontStyle::Light => FontStyle::Regular,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::MediumItalic => match rhs {
-            FontStyle::Regular => FontStyle::MediumItalic,
-            FontStyle::Italic => FontStyle::Medium,
-            FontStyle::Medium => FontStyle::Italic,
-            FontStyle::MediumItalic => FontStyle::Regular,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::Black => match rhs {
-            FontStyle::Regular => FontStyle::Black,
-            FontStyle::Black => FontStyle::Regular,
-            other => panic!("Incorrect intersection from {lhs} by {other}"),
-        },
-        FontStyle::BlackItalic => match rhs {
-            FontStyle::Regular => FontStyle::BlackItalic,
-            FontStyle::Black => FontStyle::Italic,
-            FontStyle::Italic => FontStyle::Black,
-            FontStyle::BlackItalic => FontStyle::Regular,
             other => panic!("Incorrect intersection from {lhs} by {other}"),
         },
     }
@@ -620,66 +372,22 @@ fn add_font_styles() {
         FontStyle::BoldItalic
     );
     assert_eq!(
-        FontStyle::Bold + FontStyle::Italic + FontStyle::ExtraBoldItalic,
-        FontStyle::ExtraBoldItalic
-    );
-    assert_eq!(
-        FontStyle::ExtraBoldItalic + FontStyle::ExtraBoldItalic,
-        FontStyle::ExtraBoldItalic
-    );
-    assert_eq!(
-        FontStyle::ExtraBold + FontStyle::ExtraBoldItalic,
-        FontStyle::ExtraBoldItalic
-    );
-    assert_eq!(FontStyle::Light + FontStyle::Italic, FontStyle::LightItalic);
-    assert_eq!(
-        FontStyle::Light + FontStyle::Italic + FontStyle::ExtraLight,
-        FontStyle::ExtraLightItalic
-    );
-    assert_eq!(
-        FontStyle::ExtraLightItalic + FontStyle::ExtraLight,
-        FontStyle::ExtraLightItalic
-    );
-    assert_eq!(
-        FontStyle::LightItalic + FontStyle::ExtraLight,
-        FontStyle::ExtraLightItalic
-    );
-    assert_eq!(
-        FontStyle::Italic + FontStyle::ExtraLight,
-        FontStyle::ExtraLightItalic
-    );
-    assert_eq!(
-        FontStyle::Light + FontStyle::ExtraLight,
-        FontStyle::ExtraLight
-    );
-    assert_eq!(FontStyle::Medium + FontStyle::Regular, FontStyle::Medium);
-    assert_eq!(
-        FontStyle::Medium + FontStyle::Italic,
-        FontStyle::MediumItalic
-    );
-    assert_eq!(
-        FontStyle::ExtraBoldItalic + FontStyle::Italic + FontStyle::Regular + FontStyle::Bold,
-        FontStyle::ExtraBoldItalic
-    );
-    assert_eq!(FontStyle::Thin + FontStyle::Italic, FontStyle::ThinItalic);
-    assert_eq!(FontStyle::Thin + FontStyle::Regular, FontStyle::Thin);
-    assert_eq!(FontStyle::Thin + FontStyle::Thin, FontStyle::Thin);
-    assert_eq!(FontStyle::Regular + FontStyle::Regular, FontStyle::Regular);
-    assert_eq!(
-        FontStyle::ExtraLightItalic + FontStyle::ExtraLightItalic,
-        FontStyle::ExtraLightItalic
-    );
-    assert_eq!(
         FontStyle::BoldItalic + FontStyle::Bold,
         FontStyle::BoldItalic
     );
-    assert_eq!(FontStyle::SemiBold + FontStyle::Bold, FontStyle::SemiBold);
-}
-
-#[test]
-#[should_panic]
-fn panicky_add_font_style() {
-    let _ = FontStyle::Bold + FontStyle::Thin;
+    assert_eq!(FontStyle::Bold + FontStyle::Bold, FontStyle::Bold);
+    assert_eq!(FontStyle::Regular + FontStyle::Bold, FontStyle::Bold);
+    assert_eq!(FontStyle::Regular + FontStyle::Italic, FontStyle::Italic);
+    assert_eq!(
+        FontStyle::Regular + FontStyle::Italic + FontStyle::Bold,
+        FontStyle::BoldItalic
+    );
+    assert_eq!(FontStyle::Regular + FontStyle::Regular, FontStyle::Regular);
+    assert_eq!(FontStyle::Italic + FontStyle::Italic, FontStyle::Italic);
+    assert_eq!(
+        FontStyle::Regular + FontStyle::BoldItalic + FontStyle::BoldItalic,
+        FontStyle::BoldItalic
+    );
 }
 
 #[test]
@@ -689,49 +397,20 @@ fn sub_font_styles() {
         FontStyle::BoldItalic - FontStyle::Italic - FontStyle::Regular,
         FontStyle::Bold
     );
+    assert_eq!(FontStyle::Regular - FontStyle::Regular, FontStyle::Regular);
+    assert_eq!(FontStyle::BoldItalic - FontStyle::Bold, FontStyle::Italic);
     assert_eq!(
-        FontStyle::ExtraBoldItalic - FontStyle::Italic - FontStyle::ExtraBold,
+        FontStyle::BoldItalic - FontStyle::Bold - FontStyle::Italic,
         FontStyle::Regular
     );
     assert_eq!(
-        FontStyle::ExtraBoldItalic - FontStyle::Italic,
-        FontStyle::ExtraBold,
-    );
-    assert_eq!(FontStyle::LightItalic - FontStyle::Italic, FontStyle::Light);
-    assert_eq!(
-        FontStyle::ExtraLightItalic - FontStyle::Italic - FontStyle::ExtraLight,
-        FontStyle::Regular
-    );
-    assert_eq!(FontStyle::ExtraLight - FontStyle::Light, FontStyle::Light);
-    assert_eq!(
-        FontStyle::ExtraLight - FontStyle::Light - FontStyle::Light,
-        FontStyle::Regular
-    );
-    assert_eq!(FontStyle::Medium - FontStyle::Regular, FontStyle::Medium);
-    assert_eq!(
-        FontStyle::MediumItalic - FontStyle::Italic,
-        FontStyle::Medium
-    );
-    assert_eq!(
-        FontStyle::ExtraBoldItalic - FontStyle::Italic - FontStyle::Regular - FontStyle::Bold,
+        FontStyle::BoldItalic - FontStyle::Regular - FontStyle::Italic,
         FontStyle::Bold
     );
-    assert_eq!(FontStyle::ThinItalic - FontStyle::Italic, FontStyle::Thin);
-    assert_eq!(
-        FontStyle::ThinItalic - FontStyle::ThinItalic,
-        FontStyle::Regular
-    );
-    assert_eq!(FontStyle::Thin - FontStyle::Regular, FontStyle::Thin);
-    assert_eq!(FontStyle::Regular - FontStyle::Regular, FontStyle::Regular);
-    assert_eq!(
-        FontStyle::ExtraLightItalic - FontStyle::ExtraLightItalic,
-        FontStyle::Regular
-    );
-    assert_eq!(FontStyle::BoldItalic - FontStyle::Bold, FontStyle::Italic);
 }
 
 #[test]
 #[should_panic]
 fn panicky_sub_font_style() {
-    let _ = FontStyle::Bold - FontStyle::Thin;
+    let _ = FontStyle::Bold - FontStyle::Italic;
 }
