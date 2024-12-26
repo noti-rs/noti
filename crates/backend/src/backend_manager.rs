7c9352e2037b1c3e82ed3cf428015f0e7e49eb75
Pavel Belyavsky <work.belyavsky.p.a@gmail.com>
2024-12-26 16:36:19 +0300

    refactor(backend): merge backend and server modules into single thread (#95)

--- crates/backend/src/backend_manager.rs
+++ crates/backend/src/backend_manager.rs
@@ -1,117 +1,80 @@
-use std::time::Duration;
-
 use crate::dispatcher::Dispatcher;
 use crate::idle_manager::IdleManager;
 
-use super::internal_messages::{BackendChannel, ServerMessage};
 use config::Config;
-use log::{debug, info};
-use shared::file_watcher::FileState;
+use dbus::{actions::Signal, notification::Notification};
+use log::debug;
 
 use super::window_manager::WindowManager;
 
 pub(crate) struct BackendManager {
-    config: Config,
     window_manager: WindowManager,
     idle_manager: IdleManager,
-    channel: BackendChannel,
 }
 
 impl BackendManager {
-    pub(crate) fn init(
-        config: Config,
-        renderer_internal_channel: BackendChannel,
-    ) -> anyhow::Result<Self> {
+    pub(crate) fn init(config: &Config) -> anyhow::Result<Self> {
         Ok(Self {
-            window_manager: WindowManager::init(&config)?,
-            idle_manager: IdleManager::init(&config)?,
-            channel: renderer_internal_channel,
-            config,
+            window_manager: WindowManager::init(config)?,
+            idle_manager: IdleManager::init(config)?,
         })
     }
 
-    pub(crate) fn run(&mut self) -> anyhow::Result<()> {
-        let mut notifications_to_create = vec![];
-        let mut notifications_to_close = vec![];
-        let mut partially_default_config = false;
-
-        debug!("Renderer: Running");
-        loop {
-            while let Ok(message) = self.channel.try_recv_from_server() {
-                match message {
-                    ServerMessage::ShowNotification(notification) => {
-                        debug!("Renderer: Received notification to render: {notification:?}");
-                        notifications_to_create.push(*notification);
-                    }
-                    ServerMessage::CloseNotification { id } => {
-                        debug!("Renderer: Received notification id {id} to close");
-                        notifications_to_close.push(id);
-                    }
-                }
-            }
-
-            if !notifications_to_create.is_empty() {
-                self.window_manager
-                    .create_notifications(notifications_to_create, &self.config)?;
-                notifications_to_create = vec![];
-                debug!("Renderer: Created notifications");
-            }
-
-            if !notifications_to_close.is_empty() {
-                self.window_manager
-                    .close_notifications(&notifications_to_close, &self.config)?;
-                notifications_to_close.clear();
-                debug!("Renderer: Closed notifications");
-            }
-
-            if !self.idle_manager.is_idled() {
-                if self.idle_manager.was_idled() {
-                    self.idle_manager.reset_idle_state();
-
-                    self.window_manager.reset_timeouts()?;
-                }
-
-                self.window_manager.remove_expired(&self.config)?;
-                self.window_manager.handle_actions(&self.config)?;
-            }
-
-            while let Some(message) = self.window_manager.pop_event() {
-                self.channel.send_to_server(message)?;
-            }
-
-            self.window_manager.dispatch()?;
-            self.idle_manager.dispatch()?;
-
-            {
-                match self.config.check_updates() {
-                    FileState::Updated => {
-                        partially_default_config = false;
-                        self.update_config()?;
-                        info!("Renderer: Detected changes of config files and updated")
-                    }
-                    FileState::NotFound if !partially_default_config => {
-                        partially_default_config = true;
-                        self.update_config()?;
-                        info!("The main or imported configuration file is not found, reverting this part to default values.");
-                    }
-                    FileState::NotFound | FileState::NothingChanged => (),
-                };
-
-                if self.window_manager.update_cache() {
-                    self.window_manager.update_by_config(&self.config)?;
-                }
-            }
-
-            std::thread::sleep(Duration::from_millis(50));
-            std::hint::spin_loop();
-        }
+    pub(crate) fn create_notification(&mut self, notification: Box<Notification>) {
+        let id = notification.id;
+        self.window_manager.create_notification(notification);
+        debug!("Backend Manager: Received notification with id {id} to append queue");
     }
 
-    fn update_config(&mut self) -> anyhow::Result<()> {
-        self.config.update();
-        self.window_manager.update_by_config(&self.config)?;
-        self.idle_manager.update_by_config(&self.config);
-        self.window_manager.reset_timeouts()?;
+    pub(crate) fn close_notification(&mut self, notification_id: u32) {
+        self.window_manager.close_notification(notification_id);
+        debug!("Backend Manager: Received notification id {notification_id} to close");
+    }
+
+    pub(crate) fn poll(&mut self, config: &Config) -> anyhow::Result<()> {
+        let Self {
+            idle_manager,
+            window_manager,
+        } = self;
+
+        if !idle_manager.is_idled() {
+            if idle_manager.was_idled() {
+                idle_manager.reset_idle_state();
+
+                window_manager.reset_timeouts()?;
+            }
+
+            window_manager.show_window(config)?;
+
+            window_manager.handle_close_notifications(config)?;
+            window_manager.remove_expired(config)?;
+
+            window_manager.handle_actions(config)?;
+        }
+
+        window_manager.dispatch()?;
+        idle_manager.dispatch()?;
+
+        if window_manager.update_cache() {
+            window_manager.update_by_config(config)?;
+        }
+
+        Ok(())
+    }
+
+    pub(crate) fn pop_signal(&mut self) -> Option<Signal> {
+        self.window_manager.pop_signal()
+    }
+
+    pub(crate) fn update_config(&mut self, config: &Config) -> anyhow::Result<()> {
+        let Self {
+            window_manager,
+            idle_manager,
+        } = self;
+
+        window_manager.update_by_config(config)?;
+        idle_manager.update_by_config(config);
+        window_manager.reset_timeouts()?;
         Ok(())
     }
 }
